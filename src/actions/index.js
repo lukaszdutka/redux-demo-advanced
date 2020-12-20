cb2b5c7366ddd60652c27dd8a3d1bfa71835563a
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-20 12:05:16 +0100

    example of redux-thunk action that returns function (rather than object) and needs asynchronicity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  const response = jsonPlaceholder.get('/posts')
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts')
+
+  dispatch({type: 'FETCH_POSTS', payload: response});
 }
