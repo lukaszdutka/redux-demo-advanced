051c3c2a055244dc632549f0ec3fc569896dfab7
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-21 21:44:37 +0100

    create reducer with switch statement; get state into PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,9 +5,12 @@ import {connect} from "react-redux";
 class PostList extends React.Component {
 
   componentDidMount() {
+    this.props.fetchPosts();
   }
 
   render() {
+    console.log(this.props.posts);
+
     return (
       <div>
         PostList
@@ -16,4 +19,8 @@ class PostList extends React.Component {
   }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = (state) => {
+  return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
