96b5e30ae33ee8b288b0adb054ed60fb731e8301
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-21 20:43:06 +0100

    wire example reducer and add rules of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
 import {combineReducers} from "redux";
+import postsReducer from "./postsReducer";
+
 
 export default combineReducers({
-  replaceMe: () => 'dummy'
+  posts: postsReducer
 });
+
+
+// rules of reducers:
+// - reducer must return any value besides 'undefined'
+// - reducer have to operate only on 'state' and 'action' inputs
+// - reducer should not reach for anything outside reducer (any API calls, anything - reducers are pure)
+// - reducer must not modify it's input 'state' object
